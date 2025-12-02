dae67c906de64b104c7627ebbc3aa8ae30aad190
brynmrgn <bryn@brynmrgn.com>
2025-12-02 20:31:25 +0000

    Fix models to use ApplicationModel instead of ApplicationRecord

--- app/models/research_briefing.rb
+++ app/models/research_briefing.rb
@@ -1,2 +1,10 @@
-class ResearchBriefing < ApplicationRecord
+class ResearchBriefing < ApplicationModel
+  attribute :id, :string
+  attribute :data_object
+
+  def initialize(id: nil, data_object: nil)
+    super
+    @id = id
+    @data_object = data_object
+  end
 end
