c49d453176109fcd60712d022a4c8cc7f440ff98
brynmrgn <bryn@brynmrgn.com>
2025-12-13 14:52:08 +0000

    Make JSON the default format, remove unused /types endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,14 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   
   RESOURCE_TYPES.each do |path, controller|
+    # RSS feed stays as RSS
     get "#{path}/feed", to: "object#feed", defaults: { format: :rss, controller_name: controller }, as: "feed_#{controller}".to_sym
-    get "#{path}", to: "object#index", defaults: { controller_name: controller }, as: controller.to_sym
-    get "#{path}/:term_type/:id", to: "object#index", defaults: { controller_name: controller }
-    get "#{path}/:id", to: "object#show", defaults: { controller_name: controller }, as: controller.singularize.to_sym, constraints: { id: /\d+/ }  end
+    
+    # Make JSON the default for index and show
+    get "#{path}", to: "object#index", defaults: { format: :json, controller_name: controller }, as: controller.to_sym
+    get "#{path}/:term_type/:id", to: "object#index", defaults: { format: :json, controller_name: controller }
+    get "#{path}/:id", to: "object#show", defaults: { format: :json, controller_name: controller }, as: controller.singularize.to_sym, constraints: { id: /\d+/ }
+  end
 
-  get "/types", to: "resource_types#index"
   root "home#index"
 end
