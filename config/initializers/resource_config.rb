fdebfa6512623ebcbca6cd364696ce02d4699695
brynmrgn <bryn@brynmrgn.com>
2025-12-25 10:11:44 +0000

    Move resource_config.rb to config/ to avoid Zeitwerk conflict

--- config/initializers/resource_config.rb
+++ config/initializers/resource_config.rb
@@ -1,4 +1,4 @@
 # config/initializers/resource_config.rb
 # Load the generated resource config
-resource_config_path = Rails.root.join('app', 'models', 'resource_config.rb')
+resource_config_path = Rails.root.join('config', 'resource_config.rb')
 require resource_config_path if File.exist?(resource_config_path)
