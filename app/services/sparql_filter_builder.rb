69f97821e4f35c66ca71ae1f7ef3127891ee0c97
brynmrgn <bryn@brynmrgn.com>
2025-12-25 09:56:46 +0000

    Add support for multiple filter parameters
    Now you can filter by multiple terms at once:
    ?topic=123&publisher=456

--- app/services/sparql_filter_builder.rb
+++ app/services/sparql_filter_builder.rb
@@ -1,74 +1,74 @@
 # app/services/sparql_filter_builder.rb
 #
 # Service responsible for building SPARQL filter clauses based on request parameters
-# Handles both query parameter and route-based filtering
+# Supports multiple filter parameters (e.g., ?topic=123&publisher=456)
 #
 class SparqlFilterBuilder
-  attr_reader :filter, :title, :term_id
-  
+  attr_reader :filter, :title
+
   def initialize(model_class, params, helpers)
     @model_class = model_class
     @params = params
     @helpers = helpers
     @filter = ""
     @title = ""
-    @term_id = nil
   end
-  
+
   # Builds filter clause based on request parameters
   # Returns self for method chaining
   #
   def build
     mappings = @model_class::TERM_TYPE_MAPPINGS
-    term_type = find_term_type(mappings)
-    
-    return self if term_type.nil?
-    
-    mapping = mappings[term_type.to_s]
-    
-    raise ArgumentError, "Invalid term_type '#{term_type}'" if mapping.nil?
-    
-    term_label = @helpers.get_term_label(@term_id)
-    filter_type = mapping[:predicate]
-    @title = ": #{mapping[:label]}: #{term_label}"
-    
-    @filter = build_filter_clause(filter_type, mapping)
-    
+    active_filters = find_active_filters(mappings)
+
+    return self if active_filters.empty?
+
+    filter_clauses = []
+    title_parts = []
+
+    active_filters.each do |term_type, term_id|
+      mapping = mappings[term_type]
+      term_label = @helpers.get_term_label(term_id)
+
+      title_parts << "#{mapping[:label]}: #{term_label}"
+      filter_clauses << build_filter_clause(mapping, term_id)
+    end
+
+    @title = ": #{title_parts.join(', ')}"
+    @filter = filter_clauses.join("\n")
+
     self
   end
-  
+
   private
-  
-  # Finds term_type from query params or route params
+
+  # Finds all active filters from query params
+  # Returns hash of { term_type => term_id }
   #
-  def find_term_type(mappings)
-    # Check query parameters first
+  def find_active_filters(mappings)
+    filters = {}
+
     mappings.keys.each do |key|
       if @params[key].present?
-        @term_id = @params[key]
-        return key
+        filters[key] = @params[key]
       end
     end
-    
-    # Fallback to route-based filtering
-    if @params['term_type'].present?
-      @term_id = @params[:id]
-      return @params['term_type']
-    end
-    
-    nil
+
+    filters
   end
-  
+
   # Builds SPARQL filter clause for the given mapping
   #
-  def build_filter_clause(filter_type, mapping)
+  def build_filter_clause(mapping, term_id)
+    filter_type = mapping[:predicate]
+
     if mapping[:nested]
-      "?item #{filter_type} ?authorResource . 
-       ?authorResource #{mapping[:nested_predicate]} ?term . 
-       FILTER (?term IN (<http://data.parliament.uk/terms/#{@term_id}>))"
+      "?item #{filter_type} ?#{term_id}_resource .
+       ?#{term_id}_resource #{mapping[:nested_predicate]} ?#{term_id}_term .
+       FILTER (?#{term_id}_term IN (<http://data.parliament.uk/terms/#{term_id}>))"
     else
-      "?item #{filter_type} ?term . 
-       FILTER (?term IN (<http://data.parliament.uk/terms/#{@term_id}>))"
+      "?item #{filter_type} ?#{term_id}_term .
+       FILTER (?#{term_id}_term IN (<http://data.parliament.uk/terms/#{term_id}>))"
     end
   end
 end
